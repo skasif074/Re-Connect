f905c399acc3fae59904c709d54d9eb921c4de2f
skasif074 <asifgaming074@gmail.com>
2025-10-24 20:07:14 +0530

    Peofile Picture Bug Update

--- frontend/src/components/FriendCard.jsx
+++ frontend/src/components/FriendCard.jsx
@@ -7,7 +7,7 @@ const FriendCard = ({ friend }) => {
       <div className="card-body p-4">
         <div className="flex items-center gap-3 mb-3">
           <div className="avatar size-12">
-            <img src={friend.profilePic} alt={friend.fullName} />
+            <img src={friend.profile} alt={friend.fullName} />
           </div>
           <h3 className="font-semibold truncate">{friend.fullName}</h3>
         </div>
