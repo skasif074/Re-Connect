14422ddba8dea8fedf2d109222a34d0014609d52
skasif074 <asifgaming074@gmail.com>
2026-05-29 22:29:53 +0530

    api fixed frontend auth

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -52,11 +52,11 @@ export async function signup(req,res){
         const token=jwt.sign({userId:newUser._id},process.env.JWT_SECRET_KEY,{
             expiresIn:"7d"
         })
-        res.cookie("jwt",token,{
-            maxAge:7*24*60*60*1000,
-            httpOnly:true,
-            sameSite:"strict",
-            secure:process.env.NODE_ENV==="production"
+        res.cookie("jwt", token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            sameSite: "none",
+            secure: true,
         })
         res.status(201).json({ success: true, user: newUser });
     }catch(error){
