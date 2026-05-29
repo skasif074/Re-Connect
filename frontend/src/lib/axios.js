765ad79cbf6e42a1f54727b762787c9ac0200906
skasif074 <asifgaming074@gmail.com>
2026-05-29 22:19:16 +0530

    api fixed frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios"
-const BASE_URL=import.meta.env.MODE === "development" ? "http://localhost:5001/api" :"/api"
-export const axiosInstance=axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true
+
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://re-connect-0prm.onrender.com/api"
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
 })
